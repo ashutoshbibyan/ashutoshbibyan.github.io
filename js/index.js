4b7d523f61f0eabd32678dd59ea0eb38c77c6ac3
Ashutosh <ashutosh2013official@gmail.com>
2021-11-21 00:30:25 +0530

    changes added navbar brand slide to change pages form send data to email

--- js/index.js
+++ js/index.js
@@ -1,13 +1,74 @@
-var myCarousel = document.getElementById('pages-carousel');
-var carousel = new bootstrap.Carousel(myCarousel);
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 
+var xDown = null;                                                        
+var yDown = null;
 
-var home = document.getElementById("home");
-var about = document.getElementById("about");
-var skills = document.getElementById("skills");
-var projects = document.getElementById("projects");
+var n = 1;
 
-home.onclick      =  ()=> {carousel.to(0); console.log(carousel)};
-about.onclick     =  ()=> {carousel.to(1)};
-skills.onclick    =  ()=> {carousel.to(2)};
-projects.onclick  =  ()=> {carousel.to(3)};
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+   
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+            if(n<5){
+
+                n++;
+            }
+            
+            console.log("right swipe"+n);
+             $(function () {
+               
+            
+                $("#pills-tab  li:nth-child("+n+") a").tab("show");
+              })
+
+        } else {
+            /* left swipe */
+            if(n>1){
+
+                n--;
+            }
+           
+            $(function () {
+               
+            
+                $("#pills-tab  li:nth-child("+n+") a").tab("show");
+              })
+            console.log("left swipe")
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+            console.log("swiped down")
+        } else { 
+            /* up swipe */
+            console.log("swiped up")
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
